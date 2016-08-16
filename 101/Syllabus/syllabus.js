7483ebd33778aa9c13bd06f4d777c96a0344886f
Scott Hill <scottahill@gmail.com>
2016-08-15 23:00:15 -0400

    xxx

--- 101/Syllabus/syllabus.js
+++ 101/Syllabus/syllabus.js
@@ -2,26 +2,33 @@ var duh;var arrow;
 var opensymbol="&#9660;"
 var closedsymbol="&#9654;"
 $(function(){
-var cno=$("#courseNumber").html()
-$("#toc").append('<div id="courseNumber"><a href="../index.html">'+cno.slice(0,cno.indexOf("<"))+'</a></div>')
-//$("#toc").append('<div id="courseNumber">'+cno.slice(0,cno.indexOf("<"))+'</div>')
-$("#toc").append("<ul></ul>")
-$("h3>a").each(function(i,el){
-var s=$(this).attr("name");
-var str='<li><a href="#'+s+'">'+s+'</a></li>'
-$("#toc>ul").append(str);
-});
-$("#toc").append('<div id="hideall">Collapse</div><div id="showall">Expand</div>')
-var triggerall=function(btn,boo){
-    $(".switch").prop("checked",boo).trigger("click");
-    $(btn).fadeOut(100).fadeIn(100);
-};
-$("#showall").click(function(){triggerall(this,false);});
-$("#hideall").click(function(){triggerall(this,true);});
-//$("#showall").click(function(){$(".switch").prop("checked",false).trigger("click");});
-//$("#hideall").click(function(){$(".switch").prop("checked",true).trigger("click");});
-$('h3').prepend('<span class="switch">'+opensymbol+'</span>');
-$('h3>span').prop("checked",true);
+    var triggerall=function(btn,boo){
+        $(".switch").prop("checked",boo).trigger("click");
+        $(btn).fadeOut(100).fadeIn(100);
+    };
+
+
+    var cno=$("#courseNumber").html();
+
+
+    $("#toc").append('<div class="courseNumber"><a href="../index.html">'+cno+'</a><br/>Syllabus</div>')
+    $("#toc").append("<ul></ul>")
+    
+    //POPULATE TABLE OF CONTENTS
+    $("h3>a,h2>a").each(function(i,el){
+        var s=$(this).attr("name");
+        var str='<li><a href="#'+s+'">'+s+'</a></li>'
+        $("#toc>ul").append(str);
+    });
+
+    //ADD COLLAPSE AND EXPAND BUTTONS
+    $("#toc").append('<div id="hideall">Collapse</div><div id="showall">Expand</div>')
+    $("#showall").click(function(){triggerall(this,false);});
+    $("#hideall").click(function(){triggerall(this,true);});
+    
+    //ADD OPEN/CLOSE SYMBOLS TO EACH H3
+    $('h3').prepend('<span class="switch">'+opensymbol+'</span>');
+    $('h3>span').prop("checked",true);
 $('.switch').click(function(){
 that=$(this);
 that.animate({"font-size":"14px"},100).animate({"font-size":"18px"},100);
